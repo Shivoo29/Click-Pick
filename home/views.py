73bb0b6e9c3bc17b82b9acd3728a20df3700f1b9
web_D <2004skj@gmail.com>
2022-12-23 12:12:17 +0530

    First Commit

---
+++ home/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+from home.models import Order
+from django.contrib import messages
+from .models import Order
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def order(request) :
+    if request.method == "POST" :
+        order_name = request.POST.get('order')
+        price = request.POST.get('price')
+        order = Order(order_name=order_name, order_price=price)
+        order.save()
+        messages.success(request,'Thank You! Your order has been placed.')
+    return render(request, 'order.html')
